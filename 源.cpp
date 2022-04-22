bf66b6a9bc8be095eed628fad7ab663256c07fea
EmsiaetKadosh <104206799+EmsiaetKadosh@users.noreply.github.com>
2022-04-22 21:00:48 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,142 @@
+/*
+* 
+* Emsiaet Kadosh Tools
+* ver 1.0.0 alpha
+* 
+*/
+#include <iostream>
+#include <array>
+#include <tuple>
+//#include <xmemory>
+#include <sstream>
+using namespace std;
+bool judge() {
+	return true;
+}
+struct Signal {// signal system
+	struct Position {
+		int start = 0;
+		int end = 0;
+		int lesah_ed = 0;
+		int lesah_dus = 0;
+		int lesah_sis = 0;
+		int lesah_gis = 0;
+	};
+	struct Finish {
+		int temp = 0;
+		int kuinp_ed = 0;
+		int kuinp_dus = 0;
+		int kuinp_sis = 0;
+		int kuinp_gis = 0;
+	};
+	Position position;
+	Finish finish;
+};// signal system
+class List {// list system
+/*private:*/public:
+	string content = "";
+	int position[1024] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	};// refers to position.
+	string type = "";
+public:
+	int length = 0;// This shows how many elements are there.Also used in append Similar to pointer. Usually tell how many elements are there.
+	void strappend(string input) {// Add elements after the list.
+		/*cout << "input >> " << input << endl;
+		cout << "input length >> " << input.length() << endl;
+		cout << "(int) input length >> " << (int)input.length() << endl;*/
+		content = content + input;// Content is the "list" but all elements are mixed together.
+		position[length+1] = input.length();// Put the length of input into the tuple(NUM). Use when draw.
+		type += "s";
+		length++;
+		return;
+	}
+	void intappend(int input) {
+		content = content + to_string(input);
+		position[length + 1] = to_string(input).length();
+		type += "i";
+		length++;
+		return;
+	}
+	void floatappend(float input) {
+		content = content + to_string(input);
+		position[length + 1] = to_string(input).length();
+		type += "f";
+		length++;
+		return;
+	}
+	void dubbleappend(double input) {
+		content = content + to_string(input);
+		position[length + 1] = to_string(input).length();
+		type += "d";
+		length++;
+		return;
+	}
+	void charappend(char input) {
+		content = content + to_string(input);
+		position[length + 1] = to_string(input).length();
+		type += "c";
+		length++;
+		return;
+	}
+	void boolappend(char input) {
+		content = content + to_string(input);
+		position[length + 1] = to_string(input).length();
+		type += "b";
+		length++;
+		return;
+	}
+	string draw = "None";
+	void drawInitialize(int draw_position) {
+		delete 
+	}
+};// list system
+
+// Until here : some basic functions
+
+
+// Below here : mathmode elements
+
+string commander(string ipt) {
+	Signal signal;
+	signal.position.end = signal.position.start;
+	while (signal.finish.kuinp_ed==0) {
+		if (signal.position.end >= ipt.length()-1) {
+			if (ipt.c_str()[signal.position.end] == ' ' or ipt.c_str()[signal.position.end]==';') {
+				signal.finish.kuinp_ed = 1;
+
+			}
+			else {
+				signal.position.end++;
+			}
+		}
+	}
+	return "0";
+}
+void ekt() {
+	string ipt;
+	cout << "EktMathMode >>> ";
+	cin >> ipt;
+	if (ipt.c_str()[0] == '\\') /* get pos = 0 of str : ipt */ {
+		commander(ipt);
+	}
+}
+
+// Until here : mathmode elements
+
+
+// Below here : main
+
+int main() {
+	/*cout << list.length << endl;
+	cout << list.type << endl;
+	cout << list.content << endl;
+	cout << list.position[0] << endl;
+	cout << list.position[1] << endl;
+	int a = 1;
+	cout << to_string(a) << endl;
+	*/
+	while (true) {
+		ekt();
+	}
+	return 0;
+}
